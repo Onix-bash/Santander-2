95ab21fbe2cfba0abdcdff71de04c23803aa8c68
Kristina Klepik <kristygomel@gmail.com>
2024-05-22 15:57:15 +0200

    edited sh

--- scripts/changedModules.sh
+++ scripts/changedModules.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-git config --global --add safe.directory /__w/mortgagesfdc-homes-crm/mortgagesfdc-homes-crm #fix for dubious ownership issue TODO check more deeply for better solution
-
+git config --global --add safe.directory "*"
 source_to_check_changes="origin/develop"
 
-if [ -n "$1" ]; then
-  source_to_check_changes=$1
-fi
+#if [ -n "$1" ]; then
+#  source_to_check_changes=$1
+#fi
 
 
 echo "Starting to look for changed modules against $source_to_check_changes..."
 # Array of your module directories
-modules=( $( cd ../src/ ;ls -1p | grep / | sed 's/^\(.*\)/\1/') )
+modules=( $( cd src/ ;ls -1p | grep / | sed 's/^\(.*\)/\1/') )
  #externalize module names
 # Base branch to compare against, adjust according to your workflow
 
@@ -35,3 +34,4 @@ else
   echo "changed_modules=${changed_modules[*]}" >> "$GITHUB_OUTPUT"
 fi
 echo
+

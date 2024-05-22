d5bfa734e0c95e5c6161aef171c998319886ab8a
Kristina Klepik <kristygomel@gmail.com>
2024-05-22 13:13:38 +0200

    edited

--- scripts/changedModules.sh
+++ scripts/changedModules.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-git config --global --add safe.directory /__w/mortgagesfdc-homes-crm/mortgagesfdc-homes-crm #fix for dubious ownership issue TODO check more deeply for better solution
-
+git config --global --add safe.directory "*"
 source_to_check_changes="origin/develop"
 
 if [ -n "$1" ]; then
@@ -11,7 +10,7 @@ fi
 
 echo "Starting to look for changed modules against $source_to_check_changes..."
 # Array of your module directories
-modules=( $( cd src/ ;ls -1p | grep / | sed 's/^\(.*\)/\1/') )
+modules=( $( cd ../src/ ;ls -1p | grep / | sed 's/^\(.*\)/\1/') )
  #externalize module names
 # Base branch to compare against, adjust according to your workflow
 

f64036e3ad5b524bba3b42e3b2a09385356a667a
Kristina Klepik <kristygomel@gmail.com>
2024-05-27 10:42:38 +0200

    edited

--- scripts/newMatrixVersion.sh
+++ scripts/newMatrixVersion.sh
@@ -1,44 +1,48 @@
 #!/usr/bin/env bash
 
+git config --global --add safe.directory "*"
+source_to_check_changes="origin/feature/deploy-test"
 acceptable_folders=(
   "LookupTable"
 )
-# Array of your module directories
-modules=( $(cd src/; ls -1p | grep / | sed 's|/$||') )
-
-git config --global --add safe.directory "*"
-source_to_check_changes="origin/feature/deploy-test"
 
 start() {
-  echo "deploy-test-pr"
+  # Array of your module directories
+  modules=( $(cd src/; ls -1p | grep / | sed 's|/$||') )
+
   git fetch origin
   # Initialize an associative array to hold the diffs by module
 
+  get_file_path() {
+    module=$1
+
+    for folder in "${acceptable_folders[@]}"; do
+      if [[ $file == src/$module/data/$folder/* && $is_set_input_version_run == false ]]; then
+        cd "$original_dir" && cd "src/$module/data/$folder" || exit 1
+        echo "Start set_input_version for module folder: '$module/$folder'"
+        set_input_version
+        is_set_input_version_run=true
+      fi
+    done
+  }
+
   original_dir=$(pwd)
-for module in "${modules[@]}"; do
-  # Flag to track if set_input_version has been called
-  is_set_input_version=false
+  for module in "${modules[@]}"; do
+    # Flag to track if set_input_version has been called for this module
+    is_set_input_version_run=false
 
-  # Get the list of changed files
-  diff=$(git diff-index --name-only $source_to_check_changes | grep "^src/$module/")
-  echo "diff: '$diff'"
-
-  # Iterate over each changed file
-  for file in $diff; do
-    # Check if the file matches the pattern "src/$module/data"
-    if [[ $file == src/$module/data* ]]; then
-      # Check if the file is in one of the acceptable folders and call the function
-      for folder in "${acceptable_folders[@]}"; do
-        if [[ $file == src/$module/data/$folder/* && $is_set_input_version == false ]]; then
-          cd "$original_dir" && cd "src/$module/data/$folder" || exit 1
-          echo "Start set_input_version for module folder: '$module/$folder'"
-          set_input_version
-          is_set_input_version=true
-        fi
-      done
-    fi
+    # Get the list of changed files
+    git_diff=$(git diff-index --name-only $source_to_check_changes | grep "^src/$module/data")
+    echo "git_diff: '$git_diff'"
+    # Iterate over each changed file
+    for file in $git_diff; do
+      # Check if the file matches the pattern "src/$module/data"
+      if [[ $file == src/$module/data* ]]; then
+        # Check if the file is in one of the acceptable folders and call the function
+        get_file_path $module
+      fi
+    done
   done
-done
 }
 
 set_input_version() {

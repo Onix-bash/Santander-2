1d586cac57f548361af998207d6462916c37ee55
Kristina Klepik <kristygomel@gmail.com>
2024-05-28 12:31:59 +0200

    edited

--- scripts/reporting.sh
+++ scripts/reporting.sh
@@ -44,7 +44,7 @@ run_scanner() {
   report_name="all-engines-$module_name-$(date +"%Y-%m-%d-%H-%M-%S").$1"
   report_output_path="$module_directory/$report_name"
 
-  sf scanner run --target $module --format $1 --eslintconfig $eslint_config_path --pmdconfig $pmd_config_path --outfile $report_output_path
+  sf scanner run --target $module --format $1 --pmdconfig $pmd_config_path --outfile $report_output_path
 }
 
 # Show Scanner Results on the Job Summary page (from CSV into GIT Markdown)

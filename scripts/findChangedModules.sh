53421c323d2446f143fde93bdec425d5383b423c
Kristina Klepik <kristygomel@gmail.com>
2024-06-03 16:44:28 +0200

    edited

--- scripts/findChangedModules.sh
+++ scripts/findChangedModules.sh
@@ -35,6 +35,7 @@ if [[ -n $DEVOPS_TEAM && -n $git_diff ]]; then
     while IFS= read -r file; do
       is_allowed=false
       for allowed_modification in "${ALLOWED_DEV_MODIFICATIONS_ARRAY[@]}"; do
+        echo "$allowed_modification"
         if [[ "$file" == "$allowed_modification"* ]]; then
           is_allowed=true
           break

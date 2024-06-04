988052a8a1f1729b0d50f001620cf24de62b14bc
Kristina <89141070+Kristy-user@users.noreply.github.com>
2024-06-04 16:06:40 +0200

    Update findChangedModules.sh

--- scripts/findChangedModules.sh
+++ scripts/findChangedModules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git config --global --add safe.directory "*"
+git config --global --add "*"
 
 source_to_check_changes="origin/develop"
 
@@ -8,7 +8,6 @@ if [ -n "$1" ]; then
   source_to_check_changes=$1
 fi
 
-github_actor="${GITHUB_ACTOR}"
 git fetch origin
 git_diff=$(git diff --name-only $source_to_check_changes | grep -v "^src/")
 
@@ -16,7 +15,6 @@ git_diff=$(git diff --name-only $source_to_check_changes | grep -v "^src/")
 if [[ -n $DEVOPS_TEAM && -n $git_diff ]]; then
 
   IFS=$'\n' read -r -d '' -a DEVOPS_ARRAY <<< "$(echo "$DEVOPS_TEAM" | sed '/^\s*$/d' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')"
-  echo "${DEVOPS_ARRAY[*]}"
   is_admin=false
 
   for member in "${DEVOPS_ARRAY[@]}"; do
@@ -25,18 +23,17 @@ if [[ -n $DEVOPS_TEAM && -n $git_diff ]]; then
       break
     fi
   done
-  
+
   # Check if user is NOT in DevOps team
   if ! $is_admin; then
-  IFS=$'\n' read -r -d '' -a ALLOWED_DEV_MODIFICATIONS_ARRAY <<< "$(echo "$ALLOWED_DEV_MODIFICATIONS" | sed '/^\s*$/d' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')"
-    # IFS=$'\n' read -r -d '' -a ALLOWED_DEV_MODIFICATIONS_ARRAY <<< "$ALLOWED_DEV_MODIFICATIONS"$'\n'
-     echo "${ALLOWED_DEV_MODIFICATIONS_ARRAY[*]}"
+    IFS=$'\n' read -r -d '' -a ALLOWED_DEV_MODIFICATIONS_ARRAY <<< "$(echo "$ALLOWED_DEV_MODIFICATIONS" | sed '/^\s*$/d' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')"
+
     while IFS= read -r file; do
       is_allowed=false
       for allowed_modification in "${ALLOWED_DEV_MODIFICATIONS_ARRAY[@]}"; do
         if [[ "$file" == "$allowed_modification"* ]]; then
-         echo "You can change '$file'."
           is_allowed=true
+          echo "Allow '$file'" 
           break
         fi
       done
@@ -56,7 +53,7 @@ modules=( $( cd src/ ;ls -1p | grep / | sed 's/^\(.*\)/\1/') )
  #externalize module names
 # Base branch to compare against, adjust according to your workflow
 
- 
+
 # Loop through each module to check for changes
 for module in "${modules[@]}"; do
     # Check if the module has changes compared to the base branch

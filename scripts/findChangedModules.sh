eec374ebfc24bb455d0cab66e80fa658d1cbfb54
Kristina <89141070+Kristy-user@users.noreply.github.com>
2024-06-04 15:49:10 +0200

    Update findChangedModules.sh

--- scripts/findChangedModules.sh
+++ scripts/findChangedModules.sh
@@ -8,40 +8,30 @@ if [ -n "$1" ]; then
   source_to_check_changes=$1
 fi
 
-echo "$ALLOWED_DEV_MODIFICATIONS"
-
 github_actor="${GITHUB_ACTOR}"
 git fetch origin
 git_diff=$(git diff --name-only $source_to_check_changes | grep -v "^src/")
-echo "$git_diff"
+
 # Check changes outside src folder
 if [[ -n $DEVOPS_TEAM && -n $git_diff ]]; then
 
   IFS=$'\n' read -r -d '' -a DEVOPS_ARRAY <<< "$DEVOPS_TEAM"$'\n'
- 
   is_admin=false
 
   for member in "${DEVOPS_ARRAY[@]}"; do
-   echo "$member"
     if [[ "$member" == "$GITHUB_ACTOR" ]]; then
       is_admin=true
       break
     fi
   done
-
+  
   # Check if user is NOT in DevOps team
   if ! $is_admin; then
     IFS=$'\n' read -r -d '' -a ALLOWED_DEV_MODIFICATIONS_ARRAY <<< "$ALLOWED_DEV_MODIFICATIONS"$'\n'
-    allow_changes=true
-
     while IFS= read -r file; do
       is_allowed=false
       for allowed_modification in "${ALLOWED_DEV_MODIFICATIONS_ARRAY[@]}"; do
-        echo "$allowed_modification"
-        echo "$file"
-       
         if [[ "$file" == "$allowed_modification"* ]]; then
-          echo "You can change '$file'."
           is_allowed=true
           break
         fi
@@ -62,7 +52,7 @@ modules=( $( cd src/ ;ls -1p | grep / | sed 's/^\(.*\)/\1/') )
  #externalize module names
 # Base branch to compare against, adjust according to your workflow
 
-
+ 
 # Loop through each module to check for changes
 for module in "${modules[@]}"; do
     # Check if the module has changes compared to the base branch
@@ -81,3 +71,4 @@ else
   echo "--- Changes detected for modules ${changed_modules[*]}"
   echo "changed_modules=${changed_modules[*]}" >> "$GITHUB_OUTPUT"
 fi
+echo

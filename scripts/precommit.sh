a5508b853f652a77ed509261a54b4093c214a111
Kristina Klepik <kristygomel@gmail.com>
2024-06-06 16:20:47 +0200

    js

--- scripts/precommit.sh
+++ scripts/precommit.sh
@@ -2,8 +2,12 @@
 FILES=$(git diff --cached --name-only --diff-filter=ACMR | sed 's| |\\ |g')
 [ -z "$FILES" ] && exit 0
 
-# Prettify all selected files
-echo "$FILES" | xargs ../node_modules/.bin/prettier --ignore-unknown --write
+if [ -z "$FILES" ]; then
+    exit 0
+fi
+
+echo "Running prettier"
+npm run prettier -- $FILES
 
 # Add back the modified/prettified files to staging
 echo "$FILES" | xargs git add

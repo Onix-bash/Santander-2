0b1557810deb1f17589026a36002166f77af14d4
Kristina Klepik <kristygomel@gmail.com>
2024-06-07 07:21:35 +0200

    edit script

--- scripts/precommit.sh
+++ scripts/precommit.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-FILES=$(git diff --cached --name-only --diff-filter=ACMR | sed 's| |\\ |g')
-echo "start '$FILES'"
+FILES=$(git diff --cached --name-only --diff-filter=ACMR | grep '^src/' | sed 's| |\\ |g')
+
+echo "Git diff files from ACMR filter: '$FILES'"
+
 if [ -z "$FILES" ]; then
     exit 0
 fi
-echo "before '$FILES'"
+
 echo "Running prettier"
-npm run prettier -- $FILES
-echo "after: '$FILES'"
+npm run prettier:pre-commit -- $FILES
+echo "after prettier: '$FILES'"
 # Add back the modified/prettified files to staging
 echo "$FILES" | xargs git add
 

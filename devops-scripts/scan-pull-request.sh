4ebfaca5fcf111b55916c2fe122546d929edf9ee
Kristina Klepik <kristygomel@gmail.com>
2024-06-13 10:13:16 +0200

    edited

--- devops-scripts/scan-pull-request.sh
+++ devops-scripts/scan-pull-request.sh
@@ -1,6 +1,6 @@
 pmd_config_path="config/scanner/pmd_config.xml"
 eslint_config_path="config/scanner/.eslintrc.json"
-source_to_check_changes="origin/develop"
+
 git config --global --add safe.directory /__w/Santander-2/Santander-2
 output_directory="output"
 mkdir -p "$output_directory"
@@ -11,7 +11,8 @@ git fetch origin "$PULL_REQUEST_BASE_REF"
 echo "PULL_REQUEST_HEAD_REF: '$PULL_REQUEST_HEAD_REF'"
 git_diff=$(git diff --name-only origin/$PULL_REQUEST_BASE_REF..origin/$PULL_REQUEST_HEAD_REF)
 
-echo "git_diff: '$git_diff'"
+echo "sf scanner run --target '$git_diff' --format json --pmdconfig '$pmd_config_path' --eslintconfig '$eslint_config_path' --outfile '$report_output_path'"
+
 report_output_path="$output_directory/report.json"
 sf scanner run --target "$git_diff" --format json --pmdconfig "$pmd_config_path" --eslintconfig "$eslint_config_path" --outfile $report_output_path
 

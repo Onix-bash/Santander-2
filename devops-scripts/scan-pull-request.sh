849ccd07b486405e5fa5f52e8912aa084cd15f57
Kristina Klepik <kristygomel@gmail.com>
2024-06-13 09:09:43 +0200

    edited

--- devops-scripts/scan-pull-request.sh
+++ devops-scripts/scan-pull-request.sh
@@ -7,7 +7,11 @@ mkdir -p "$output_directory"
 git fetch origin "$PULL_REQUEST_HEAD_REF"
 git fetch origin "$PULL_REQUEST_BASE_REF"
 
+
 git_diff=git diff --name-only origin/${{ env.PULL_REQUEST_BASE_REF }}..origin/${{ env.PULL_REQUEST_HEAD_REF }}
 
 echo "git_diff: '$git_diff'"
-sf scanner run --target $git_diff --format csv --pmdconfig $pmd_config_path --eslintconfig $eslint_config_path --outfile $report_output_path
+
+sf scanner run --target "$git_diff" --format json --pmdconfig "$pmd_config_path" --eslintconfig "$eslint_config_path" --outfile output_directory/report.json
+
+cat output_directory/report.json

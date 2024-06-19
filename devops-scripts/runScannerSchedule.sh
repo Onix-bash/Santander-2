eaa34018a472f120fc82cf4bfaab7e34da12f222
Kristina Klepik <kristygomel@gmail.com>
2024-06-19 16:47:33 +0200

    added cpd engine

--- devops-scripts/runScannerSchedule.sh
+++ devops-scripts/runScannerSchedule.sh
@@ -48,7 +48,7 @@ run_scanner() {
   report_name="all-engines-$module_name-$(date +"%Y-%m-%d-%H-%M-%S").$1"
   report_output_path="$module_directory/$report_name"
 
-  sf scanner run --target $module --format $1 --pmdconfig $pmd_config_path --eslintconfig $eslint_config_path --engine cpd --outfile $report_output_path
+  sf scanner run --target $module --format $1 --pmdconfig $pmd_config_path --eslintconfig $eslint_config_path --engine pmd,eslint,cpd --outfile $report_output_path
 }
 
 # Show Scanner Results on the Job Summary page (from CSV into GIT Markdown)

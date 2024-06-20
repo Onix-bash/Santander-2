0ae376c407e1cc593cf0401760c409c7e69bae8d
Kristina Klepik <kristygomel@gmail.com>
2024-06-20 15:22:59 +0200

    rollback ignored_modules

--- devops-scripts/test_violation.sh
+++ devops-scripts/test_violation.sh
@@ -15,10 +15,3 @@ echo "${source_paths_to_scan[@]}"
 # Run scanner using All engines with custom PMD config
 mkdir -p "output"
 sf scanner:run --target "${source_paths_to_scan[@]}" --severity-threshold=2 --verbose-violations --format json --pmdconfig "config/scanner/pmd_config.xml" --outfile output/report.json
-
-# Read the JSON output and format it
-JSON_OUTPUT=$(cat output/report.json)
-FORMATTED_JSON=$(echo "$JSON_OUTPUT" | jq 'walk(if type == "object" and .message? then .message |= gsub("\\n"; "") else . end)')
-
-# Output the scan report to the console
-echo "$FORMATTED_JSON"

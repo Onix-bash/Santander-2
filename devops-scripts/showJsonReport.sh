02782e6207f5a131ffd97390d82ed68205077684
Kristina Klepik <kristygomel@gmail.com>
2024-06-20 15:42:17 +0200

    edited

--- devops-scripts/showJsonReport.sh
+++ devops-scripts/showJsonReport.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Read the JSON output and format it
 JSON_OUTPUT=$(cat output/report.json)
+echo "$JSON_OUTPUT"
 FORMATTED_JSON=$(echo "$JSON_OUTPUT" | jq 'walk(if type == "object" and .message? then .message |= gsub("\\n"; "") else . end)')
 
 # Output the scan report to the console

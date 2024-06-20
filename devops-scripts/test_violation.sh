e2d58287cfffc442188710700cebafc1ebc11430
Kristina Klepik <kristygomel@gmail.com>
2024-06-20 14:50:51 +0200

    rollback ignored_modules

--- devops-scripts/test_violation.sh
+++ devops-scripts/test_violation.sh
@@ -15,6 +15,12 @@ echo "${source_paths_to_scan[@]}"
 # Run scanner using All engines with custom PMD config
 mkdir -p "output"
 sf scanner:run --target "${source_paths_to_scan[@]}" --severity-threshold=2 --verbose-violations --format json --pmdconfig "config/scanner/pmd_config.xml" --outfile output/report.json
+
+# Read the JSON output and format it
 JSON_OUTPUT=$(cat output/report.json)
 FORMATTED_JSON=$(echo "$JSON_OUTPUT" | jq '.')
-echo "report=${FORMATTED_JSON}" >> "$GITHUB_OUTPUT"
+
+# Write the formatted JSON to the GITHUB_OUTPUT environment file
+echo "report<<EOF" >> "$GITHUB_OUTPUT"
+echo "$FORMATTED_JSON" >> "$GITHUB_OUTPUT"
+echo "EOF" >> "$GITHUB_OUTPUT"

cfb26f2213436865ac44b26cf81cb0dcd6e7cca2
Kristina <89141070+Kristy-user@users.noreply.github.com>
2024-06-21 08:24:58 +0200

    Update test_violation.sh

--- devops-scripts/test_violation.sh
+++ devops-scripts/test_violation.sh
@@ -14,4 +14,20 @@ fi
 echo "${source_paths_to_scan[@]}"
 # Run scanner using All engines with custom PMD config
 mkdir -p "output"
-sf scanner:run --target "${source_paths_to_scan[@]}" --severity-threshold=2 --verbose-violations --format json --pmdconfig "config/scanner/pmd_config.xml" --outfile output/report.json
+sf scanner:run --target "${source_paths_to_scan[@]}" --severity-threshold=$SEVERITY --verbose-violations --format json --pmdconfig "config/scanner/pmd_config.xml" --outfile output/report.json
+
+# Read the JSON output and format it
+JSON_OUTPUT=$(cat output/report.json)
+
+# Remove newline characters from message field and filter by severity
+FILTERED_JSON=$(echo "$JSON_OUTPUT" | jq --arg severity "$SEVERITY" '[.[] | {engine, fileName, violations: [.violations[] | select(.severity <= ($severity | tonumber)) | .message |= gsub("\\n"; "") ]} | select(.violations | length > 0)]')
+
+# Output the scan report to the console
+echo "$FILTERED_JSON"
+
+# Exit with an error if there are any violations with severity
+SEVERITY_COUNT=$(echo "$FILTERED_JSON" | jq 'map(.violations) | flatten | length')
+if [ "$SEVERITY_COUNT" -gt 0 ]; then
+  echo "There are $SEVERITY_COUNT violations with severity $SEVERITY or lower."
+  exit 1
+fi

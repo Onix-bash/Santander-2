13c286d6d758e497f885af4cb9983eb0cb692a5f
Kristina Klepik <kristygomel@gmail.com>
2024-07-04 15:18:34 +0200

    edited

--- devops-scripts/runScanner.sh
+++ devops-scripts/runScanner.sh
@@ -5,7 +5,8 @@
 
 source_paths_to_scan=()
 IFS=$'\n' read -r -d '' -a IGNORED_MODULES_ARRAY <<< "$(echo "$IGNORED_MODULES" | sed '/^\s*$/d' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')"
-
+echo "$IGNORED_MODULES"
+echo "$SEVERITY"
 start() {
   if [ -n "$1" ]; then
     filter_modules_to_scan "$@"
@@ -45,6 +46,7 @@ filter_modules_to_scan() {
   local modules_to_scan=("$@")
   echo "${modules_to_scan[@]}"
   for module in "${modules_to_scan[@]}"; do
+    echo "${IGNORED_MODULES_ARRAY[*]}"
      if ! [[ "${IGNORED_MODULES_ARRAY[*]}" =~ (^|[[:space:]])"$module"($|[[:space:]]) ]]; then
        source_paths_to_scan+=("src/$module")
     fi

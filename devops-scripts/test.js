d490049a80f4a76c76e478af5a2c906fe48ca567
Kristina Klepik <kristygomel@gmail.com>
2024-07-12 15:19:51 +0200

    edited

---
+++ devops-scripts/test.js
@@ -0,0 +1,16 @@
+const argv = require('yargs-parser')(process.argv.slice(2))
+const {execSync} = require('child_process');
+console.log(argv.baseBranch)
+
+const sourceToCheckChanges = argv.baseBranch ? argv.baseBranch : `origin/${GITHUB_BASE_REF}`;
+const currentBranch = `origin/${GITHUB_HEAD_REF}`;
+
+
+console.log(sourceToCheckChanges,currentBranch )
+execSync('git fetch origin');
+        const gitDiff = execSync(`git diff --name-only ${sourceToCheckChanges}...${currentBranch}`)
+            .toString()
+            .split('\n')
+            .map((filename => filename.split('/').slice(-1).join()));
+
+        console.log(gitDiff);

e04949eac829ee4cf291651eb2d8f47752e65e68
Kristina Klepik <kristygomel@gmail.com>
2024-07-12 16:08:23 +0200

    edited

--- devops-scripts/test.js
+++ devops-scripts/test.js
@@ -1,9 +1,9 @@
 const argv = require('yargs-parser')(process.argv.slice(2))
 const {execSync} = require('child_process');
-console.log(argv.baseBranch)
+
 const GITHUB_HEAD_REF = process.env.GITHUB_HEAD_REF;
 const GITHUB_BASE_REF = process.env.GITHUB_BASE_REF;
-console.log(GITHUB_HEAD_REF,GITHUB_BASE_REF)
+console.log('head', GITHUB_HEAD_REF,'base', GITHUB_BASE_REF)
 
 const GITHUB_BASE_BRANCH = argv.baseBranch ? argv.baseBranch : `origin/${GITHUB_BASE_REF}`;
 const GITHUB_CURRENT_BRANCH = argv.baseBranch ? 'origin' : `origin/${GITHUB_HEAD_REF}`;

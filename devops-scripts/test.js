e7834ef0421607cccbfdcdf3c0ece840b7025d95
Kristina Klepik <kristygomel@gmail.com>
2024-07-12 15:34:59 +0200

    edited

--- devops-scripts/test.js
+++ devops-scripts/test.js
@@ -1,15 +1,15 @@
 const argv = require('yargs-parser')(process.argv.slice(2))
 const {execSync} = require('child_process');
 console.log(argv.baseBranch)
-const GITHUB_BASE_REF = process.env.GITHUB_BASE_REF;
 const GITHUB_HEAD_REF = process.env.GITHUB_HEAD_REF;
-const sourceToCheckChanges = argv.baseBranch ? argv.baseBranch : `origin/${GITHUB_BASE_REF}`;
-const currentBranch = `origin/${GITHUB_HEAD_REF}`;
+const GITHUB_BASE_REF = process.env.GITHUB_BASE_REF;
+console.log(GITHUB_HEAD_REF,GITHUB_BASE_REF)
 
+const GITHUB_BASE_BRANCH = argv.baseBranch ? argv.baseBranch : `origin/${GITHUB_BASE_REF}`;
+const GITHUB_CURRENT_BRANCH = `origin/${GITHUB_HEAD_REF}`;
 
-console.log(sourceToCheckChanges,currentBranch )
 execSync('git fetch origin');
-        const gitDiff = execSync(`git diff --name-only ${sourceToCheckChanges}...${currentBranch}`)
+        const gitDiff = execSync(`git diff --name-only ${GITHUB_BASE_BRANCH}...${GITHUB_CURRENT_BRANCH}`)
             .toString()
             .split('\n')
             .map((filename => filename.split('/').slice(-1).join()));

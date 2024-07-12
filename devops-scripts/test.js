2fadb22414abf241a372ba00f2e7307c898aa846
Kristina Klepik <kristygomel@gmail.com>
2024-07-12 16:26:21 +0200

    edited

--- devops-scripts/test.js
+++ devops-scripts/test.js
@@ -5,8 +5,8 @@ const GITHUB_HEAD_REF = process.env.GITHUB_HEAD_REF;
 const GITHUB_BASE_REF = process.env.GITHUB_BASE_REF;
 console.log('head', GITHUB_HEAD_REF,'base', GITHUB_BASE_REF)
 
-const GITHUB_BASE_BRANCH = argv.baseBranch ? argv.baseBranch : `origin/${GITHUB_BASE_REF}`;
-const GITHUB_CURRENT_BRANCH = argv.baseBranch ? 'origin' : `origin/${GITHUB_HEAD_REF}`;
+const GITHUB_BASE_BRANCH = GITHUB_HEAD_REF ? `origin/${GITHUB_BASE_REF}` : 'HEAD^'
+const GITHUB_CURRENT_BRANCH = GITHUB_BASE_REF ? `origin/${GITHUB_HEAD_REF}` : 'origin';
 
 execSync('git fetch origin');
         const gitDiff = execSync(`git diff --name-only ${GITHUB_BASE_BRANCH}...${GITHUB_CURRENT_BRANCH}`)

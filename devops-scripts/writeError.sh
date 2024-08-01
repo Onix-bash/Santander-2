f93de16571cc1b6d08c0d615ffe5affd964d3463
Kristina Klepik <kristygomel@gmail.com>
2024-08-01 14:45:31 +0200

    edited

--- devops-scripts/writeError.sh
+++ devops-scripts/writeError.sh
@@ -7,9 +7,10 @@ echo "Head Ref: $GITHUB_HEAD_REF" >> error_info.txt
 echo "Base Ref: $GITHUB_BASE_REF" >> error_info.txt
 echo "Repository: $GITHUB_REPOSITORY" >> error_info.txt
 echo "Run ID: $GITHUB_RUN_ID" >> error_info.txt
-echo "Edited Modules: $changed_files" >> error_info.txt # Example file path
+echo "Edited Modules: ${changed_files[*]}" >> error_info.txt # Example file path
 echo "Show file"
 cat error_info.txt
+
 # Read the file content to a variable
-#            COMMENTS=$(cat error_info.txt)
-#            sf data create record --sobject Log__c --values "OrganizationEnvironmentType__c='Scratch Org' Comments__c='$COMMENTS'"
+COMMENTS=$(cat error_info.txt)
+sf data create record --sobject Log__c --values "OrganizationEnvironmentType__c='Scratch Org' Comments__c='$COMMENTS'"

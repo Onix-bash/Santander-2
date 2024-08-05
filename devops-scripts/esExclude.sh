3a7eb11cfb45d8f0358b2bedfacde94c460bb36a
Kristina Klepik <kristygomel@gmail.com>
2024-08-05 15:28:15 +0200

    add

--- devops-scripts/esExclude.sh
+++ devops-scripts/esExclude.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-source_to_check_changes="origin/$GITHUB_BASE_REF"
+if [ -n "$GITHUB_BASE_REF" ]; then
+  source_to_check_changes="origin/$GITHUB_BASE_REF"
+  else
+  source_to_check_changes="HEAD^"
+fi
+
+if [ -n "$GITHUBa_HEAD_REF" ]; then
+  source_to_check_changes="origin/$GITHUB_HEAD_REF"
+  else
+  source_to_check_changes=""
+fi
+
 ES_PATH='src/decision-centre/main/default/expressionSetDefinition'
 
 git fetch origin
-changed_es_files=$(git diff --name-only $source_to_check_changes $ES_PATH)
+changed_es_files=$(git diff --name-only $source_to_check_changes...$current_branch $ES_PATH)
 
 for file_path in $changed_es_files; do
   echo "!$file_path" >> .forceignore
 done
-
-cat .forceignore

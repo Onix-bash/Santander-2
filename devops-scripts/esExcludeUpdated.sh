5aae4b26c4cbe8ebecb812e5483ec9e0098b952a
Kristina Klepik <kristygomel@gmail.com>
2024-08-07 12:50:09 +0200

    edited

--- devops-scripts/esExcludeUpdated.sh
+++ devops-scripts/esExcludeUpdated.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-# Default branches to compare
 source_to_check_changes="origin/$GITHUB_BASE_REF"
 current_branch="origin/$GITHUB_HEAD_REF"
 
-# Override if an argument is passed
 if [ -n "$1" ]; then
-  source_to_check_changes="$1"
+  source_to_check_changes=$1
+  current_branch=""
 fi
 
 ES_PATH='^src/.*/expressionSetDefinition/'
 
-# Fetch both branches
-git fetch origin $GITHUB_BASE_REF
-git fetch origin $GITHUB_HEAD_REF
+git fetch origin
+changed_es_files=$(git diff --name-only $source_to_check_changes..$current_branch | grep -E $ES_PATH)
 
-# Get the list of changed files matching the pattern
-changed_es_files=$(git diff --name-only $source_to_check_changes...$current_branch | grep -E $ES_PATH)
+changed_all=$(git diff --name-only "$source_to_check_changes")
+echo "all: $changed_all"
 
-# Append changes to .forceignore
 for file_path in $changed_es_files; do
   echo "!$file_path" >> .forceignore
   echo "changed: $file_path"

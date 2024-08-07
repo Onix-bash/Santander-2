d79ea69dd090a7a38deed08fcb41855090bea4ef
Kristina Klepik <kristygomel@gmail.com>
2024-08-07 13:02:34 +0200

    edited

--- devops-scripts/esExcludeUpdated.sh
+++ devops-scripts/esExcludeUpdated.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source_to_check_changes="origin/$GITHUB_BASE_REF"
-current_branch="origin/$GITHUB_HEAD_REF"
+source_to_check_changes="$GITHUB_BASE_REF"
+current_branch="$GITHUB_HEAD_REF"
 
 if [ -n "$1" ]; then
   source_to_check_changes=$1
@@ -11,7 +11,6 @@ echo "$source_to_check_changes"
 echo "$current_branch"
 ES_PATH='^src/.*/expressionSetDefinition/'
 
-git fetch origin
 changed_es_files=$(git diff --name-only $source_to_check_changes..$current_branch | grep -E $ES_PATH)
 
 changed_all=$(git diff --name-only "$source_to_check_changes" | grep -E $ES_PATH)

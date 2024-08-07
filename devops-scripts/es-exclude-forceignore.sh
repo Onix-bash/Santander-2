34dba28d88ce077d06b727cd2319cc24525aae0c
Kristina Klepik <kristygomel@gmail.com>
2024-08-07 14:18:36 +0200

    =

--- devops-scripts/es-exclude-forceignore.sh
+++ devops-scripts/es-exclude-forceignore.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
-echo "GITHUB_BASE_BRANCH: $GITHUB_BASE_BRANCH"
-echo "GITHUB_CURRENT_BRANCH: $GITHUB_CURRENT_BRANCH"
+git config --global --add safe.directory "*"
+source_to_check_changes="origin/$GITHUB_BASE_REF"
+current_branch="origin/$GITHUB_HEAD_REF"
 
-if [ -n "$GITHUB_BASE_REF" ]; then
-  source_to_check_changes="origin/$GITHUB_BASE_REF"
-  else
-  source_to_check_changes="HEAD^"
-fi
-
-if [ -n "$GITHUBa_HEAD_REF" ]; then
-  current_branch="origin/$GITHUB_HEAD_REF"
-  else
+if [ -n "$1" ]; then
+  source_to_check_changes=$1
   current_branch=""
 fi
-echo "source_to_check_changes: $source_to_check_changes"
-echo "current_branch: $current_branch"
 
 ES_PATH='src/decision-centre/main/default/expressionSetDefinition'
 
 git fetch origin
-changed_es_files=$(git diff --name-only $source_to_check_changes...$current_branch $ES_PATH)
-echo "changed_es_files: $changed_es_files"
+changed_es_files=$(git diff --name-only $source_to_check_changes $ES_PATH)
+
 for file_path in $changed_es_files; do
   echo "!$file_path" >> .forceignore
 done
 
 cat .forceignore
-
-

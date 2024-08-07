ea3f3079dd0cd6fa72fed41b6cd7a3be22844e6d
Kristina Klepik <kristygomel@gmail.com>
2024-08-07 14:26:16 +0200

    edited

--- devops-scripts/esExcludeUpdated.sh
+++ devops-scripts/esExcludeUpdated.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+git config --global --add safe.directory "*"
 source_to_check_changes="origin/$GITHUB_BASE_REF"
 current_branch="origin/$GITHUB_HEAD_REF"
 
@@ -14,15 +14,14 @@ ES_PATH='^src/.*/expressionSetDefinition/'
 git fetch origin
 # Get the list of changed files that match the pattern
 changed_es_files=$(git diff --name-only $source_to_check_changes...$current_branch | grep -E $ES_PATH)
-echo "Changed files matching pattern: $changed_es_files"
+echo "changed_es_files: $changed_es_files"
 
 # Get the list of all changed files from the source branch
 changed_all=$(git diff --name-only $source_to_check_changes..$current_branch)
-echo "All changed files: $changed_all"
+echo "changed_all: $changed_all"
 
-# Get the list of changed files in the latest commit that match the pattern
-changed_head=$(git diff --name-only  | grep -E $ES_PATH)
-echo "Changed files in HEAD matching pattern: $changed_head"
+all=$(git diff --name-only $source_to_check_changes)
+echo "all: $all"
 
 for file_path in $changed_es_files; do
   echo "!$file_path" >> .forceignore

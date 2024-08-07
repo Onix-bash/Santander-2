3b7480fd25bf506f33bf95b5dcefe035e02f6361
Kristina Klepik <kristygomel@gmail.com>
2024-08-07 14:11:52 +0200

    edited

--- devops-scripts/esExcludeUpdated.sh
+++ devops-scripts/esExcludeUpdated.sh
@@ -13,15 +13,15 @@ ES_PATH='^src/.*/expressionSetDefinition/'
 
 git fetch origin
 # Get the list of changed files that match the pattern
-changed_es_files=$(git diff --name-only $source_to_check_changes $current_branch | grep -E $ES_PATH)
+changed_es_files=$(git diff --name-only $source_to_check_changes...$current_branch | grep -E $ES_PATH)
 echo "Changed files matching pattern: $changed_es_files"
 
 # Get the list of all changed files from the source branch
-changed_all=$(git diff --name-only $source_to_check_changes)
+changed_all=$(git diff --name-only $source_to_check_changes..$current_branch)
 echo "All changed files: $changed_all"
 
 # Get the list of changed files in the latest commit that match the pattern
-changed_head=$(git diff --name-only HEAD^ HEAD | grep -E $ES_PATH)
+changed_head=$(git diff --name-only  | grep -E $ES_PATH)
 echo "Changed files in HEAD matching pattern: $changed_head"
 
 for file_path in $changed_es_files; do

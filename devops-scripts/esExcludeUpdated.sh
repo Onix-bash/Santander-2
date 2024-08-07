19b16e3334fd6d5f7f514ef193bcc0da1f72d1a4
Kristina Klepik <kristygomel@gmail.com>
2024-08-07 13:23:19 +0200

    edited

--- devops-scripts/esExcludeUpdated.sh
+++ devops-scripts/esExcludeUpdated.sh
@@ -11,15 +11,18 @@ echo "source_to_check_changes $source_to_check_changes"
 echo "current_branch $current_branch"
 ES_PATH='^src/.*/expressionSetDefinition/'
 
-#git fetch origin
-changed_es_files=$(git diff --name-only $source_to_check_changes...$current_branch | grep -E $ES_PATH)
-echo "current: $changed_es_files"
+git fetch origin
+# Get the list of changed files that match the pattern
+changed_es_files=$(git diff --name-only $source_to_check_changes $current_branch | grep -E $ES_PATH)
+echo "Changed files matching pattern: $changed_es_files"
 
-changed_all=$(git diff --name-only "$source_to_check_changes")
-echo "all: $changed_all"
+# Get the list of all changed files from the source branch
+changed_all=$(git diff --name-only $source_to_check_changes)
+echo "All changed files: $changed_all"
 
-changed_head=$(git diff "HEAD^" | grep -E $ES_PATH)
-echo "head: $changed_changed_head"
+# Get the list of changed files in the latest commit that match the pattern
+changed_head=$(git diff --name-only HEAD^ HEAD | grep -E $ES_PATH)
+echo "Changed files in HEAD matching pattern: $changed_head"
 
 for file_path in $changed_es_files; do
   echo "!$file_path" >> .forceignore

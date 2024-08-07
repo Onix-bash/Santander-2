af5b0900a34497426f3a48b386d390b0fa436628
Kristina Klepik <kristygomel@gmail.com>
2024-08-07 12:24:17 +0200

    edited

--- devops-scripts/esExcludeUpdated.sh
+++ devops-scripts/esExcludeUpdated.sh
@@ -11,7 +11,7 @@ ES_PATH='^src/.*/expressionSetDefinition/'
 
 git fetch origin
 changed_es_files=$(git diff --name-only $source_to_check_changes...$current_branch | grep -E $ES_PATH)
-echo "$changed_es_files"
+echo "changed: $changed_es_files"
 for file_path in $changed_es_files; do
   echo "!$file_path" >> .forceignore
 done

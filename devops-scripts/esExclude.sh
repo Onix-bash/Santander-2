37d6be9e42ffa17ffdc407d82e6651ce6e36cd1a
Kristina Klepik <kristygomel@gmail.com>
2024-08-07 11:25:54 +0200

    edited

--- devops-scripts/esExclude.sh
+++ devops-scripts/esExclude.sh
@@ -5,7 +5,7 @@ source_to_check_changes="origin/$GITHUB_BASE_REF"
 current_branch="origin/$GITHUB_HEAD_REF"
 
 if [[ -n "$1" && "$1" != null ]]; then
-  echo "show null"
+  echo "merge to develop"
   source_to_check_changes=$1
   current_branch=""
 fi
@@ -14,7 +14,14 @@ echo "2: $2"
 ES_PATH='^src/.*/expressionSetDefinition/'
 
 git fetch origin
-changed_es_files=$(git diff --name-only $source_to_check_changes...$current_branch | grep -E $ES_PATH)
+if [[ -n "$2" && "$2" == 'scratch' ]]; then
+  echo "if scratch"
+  changed_es_files=$(git diff --name-only $source_to_check_changes | grep -E $ES_PATH)
+  else
+  echo "not scratch"
+  changed_es_files=$(git diff --name-only $source_to_check_changes...$current_branch | grep -E $ES_PATH)
+fi
+
 echo "$changed_es_files"
 for file_path in $changed_es_files; do
   echo "!$file_path" >> .forceignore

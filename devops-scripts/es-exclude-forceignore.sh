54b6fdd5eaf3983cde67506d5e67a8947ef4b9cb
Kristina Klepik <kristygomel@gmail.com>
2024-08-09 15:07:54 +0200

    Update es-exclude-forceignore.sh

--- devops-scripts/es-exclude-forceignore.sh
+++ devops-scripts/es-exclude-forceignore.sh
@@ -3,7 +3,8 @@
 git config --global --add safe.directory "*"
 source_to_check_changes="origin/$GITHUB_BASE_REF"
 current_branch="origin/$GITHUB_HEAD_REF"
-
+echo "$source_to_check_changes"
+echo "$current_branch"
 if [ -n "$1" ]; then
   source_to_check_changes=$1
   current_branch=""
@@ -16,7 +17,7 @@ fi
 ES_PATH='src/decision-centre/main/default/expressionSetDefinition'
 
 git fetch origin
-changed_es_files=$(git diff --name-only $source_to_check_changes $ES_PATH)
+changed_es_files=$(git diff --name-only $source_to_check_changes...$current_branch $ES_PATH)
 echo "changed_es_files: $changed_es_files"
 
 for file_path in $changed_es_files; do

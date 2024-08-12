4a290fd99502309c276c257bdd6b49654bf47e73
Kristina Klepik <kristygomel@gmail.com>
2024-08-12 15:42:47 +0200

    edited

--- devops-scripts/es-exclude-forceignore.sh
+++ devops-scripts/es-exclude-forceignore.sh
@@ -9,12 +9,12 @@ if [ -n "$1" ]; then
 fi
 
 if [ -n "$2" ]; then
-    git fetch origin develop:develop
+    git fetch origin $2
     current_branch=$2
 fi
 
 ES_PATH='^src/.*/expressionSetDefinition/'
-
+git fetch origin
 changed_es_files=$(git diff --name-only $source_to_check_changes...$current_branch | grep -E $ES_PATH)
 
 for file_path in $changed_es_files; do

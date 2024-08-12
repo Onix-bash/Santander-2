5a7314e4bae1b21ecae9182560f5c3810b7881ed
Kristina Klepik <kristygomel@gmail.com>
2024-08-12 08:40:23 +0200

    edited

--- devops-scripts/es-exclude-forceignore.sh
+++ devops-scripts/es-exclude-forceignore.sh
@@ -9,7 +9,7 @@ if [ -n "$1" ]; then
 fi
 
 if [ -n "$2" ]; then
-#    git fetch origin develop:develop
+    git fetch origin develop
     source_to_check_changes="develop"
     current_branch=$2
 fi

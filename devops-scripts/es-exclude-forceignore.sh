540e7c3075e71f54671ec727e4ae7301e631d362
Kristina <89141070+Kristy-user@users.noreply.github.com>
2024-08-12 14:12:17 +0200

    Update es-exclude-forceignore.sh

--- devops-scripts/es-exclude-forceignore.sh
+++ devops-scripts/es-exclude-forceignore.sh
@@ -12,11 +12,12 @@ if [ -n "$2" ]; then
     git fetch origin develop:develop
     current_branch=$2
 fi
-
+echo "$source_to_check_changes"
+echo "$current_branch"
 ES_PATH='^src/.*/expressionSetDefinition/'
 
 changed_es_files=$(git diff --name-only $source_to_check_changes...$current_branch | grep -E $ES_PATH)
-
+echo "$changed_es_files"
 for file_path in $changed_es_files; do
   echo "!$file_path" >> .forceignore
 done

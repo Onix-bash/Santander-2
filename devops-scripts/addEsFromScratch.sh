cf4e3dc8099549d73712a843dbeabf1875e7bfef
Kristina Klepik <kristygomel@gmail.com>
2024-08-14 09:42:02 +0200

    Edited

--- devops-scripts/addEsFromScratch.sh
+++ devops-scripts/addEsFromScratch.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-# Check if any files are detected
-echo "$1"
-if [ -z "$1" ]; then
-    echo "No differences found between CI-Org and develop."
-else
-    echo "Differences found in: $1"
-
+if [ -n "$1" ]; then
     # Add the changed files to .forceignore with "!" prefix
     for file in $1; do
         # Check if the entry already exists in .forceignore
         if ! grep -qx "!$file" .forceignore; then
             echo "!$file" >> .forceignore
-            echo "Added !$file to .forceignore"
         fi
     done
 fi

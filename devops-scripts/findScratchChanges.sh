03cb6b7f3c5d1848f7bd1bcf2ac3f9f4996cf803
Kristina Klepik <kristygomel@gmail.com>
2024-08-14 09:46:27 +0200

    Edited

---
+++ devops-scripts/findScratchChanges.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#sf project retrieve start --metadata ExpressionSetDefinition --output-dir scratch_es --ignore-conflicts
+
+DIR1="scratch_es/main/default/expressionSetDefinition"
+DIR2="src/decision-centre/main/default/expressionSetDefinition"
+
+# Run the diff command and get the list of changed files
+changed_files=$(diff -qr "$DIR1" "$DIR2" | grep -E '^Files ' | awk '{print $2}' | sed "s|^$DIR1/||")
+
+# Format the changed files to be space-separated on a single line
+formatted_changed_files=$(echo "$changed_files" | tr '\n' ' ')
+echo "$formatted_changed_files"
+echo "changed_files=$formatted_changed_files" >> "$GITHUB_OUTPUT"

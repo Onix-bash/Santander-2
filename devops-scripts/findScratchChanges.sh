a9ed4cd8c4d1340ec2dbf04a87731f5ef8ffeb47
Kristina Klepik <kristygomel@gmail.com>
2024-08-14 13:13:24 +0200

    Edited

--- devops-scripts/findScratchChanges.sh
+++ devops-scripts/findScratchChanges.sh
@@ -7,7 +7,7 @@ git stash push -m "temporary stash" && git checkout origin/develop
 #sf project retrieve start --metadata ExpressionSetDefinition --output-dir scratch_es --ignore-conflicts
 
 DIR1="scratch_es/main/default/expressionSetDefinition"
-DIR2="src/decision-centre/main/default/expressionSetDefinition"
+DIR2="src"
 
 ## Check if the retrieval command was successful
 if [ ! -d "$DIR1" ] || [ -z "$(ls -A "$DIR1")" ]; then
@@ -16,7 +16,6 @@ fi
 
 changed_files=$(diff -qr "$DIR1" "$DIR2" | grep -E '^Files ' | awk '{print $2}' | sed "s|^$DIR1/||")
 
-# Check if there are changed files to deploy
 if [ -n "$changed_files" ]; then
     echo "Differences found in:"
     echo "$changed_files"
@@ -26,14 +25,19 @@ if [ -n "$changed_files" ]; then
 
     # Step 7: Clean the manifest to include only changed files
     for file in $changed_files; do
-        xmlstarlet ed -L \
-            -d "//members[. != '*'][not(. = '${file%%/*}')] | //members[. = '*']" \
-            esManifest/package.xml
+        # Ensure the manifest file exists before attempting to modify it
+        if [ -f "expressionSetManifest.xml" ]; then
+            xmlstarlet ed -L \
+                -d "//members[. != '*'][not(. = '${file%%/*}')] | //members[. = '*']" \
+                expressionSetManifest.xml
+        fi
     done
 
     # Step 8: Deploy the changed files using the cleaned manifest
-#    sf project deploy start --manifest expressionSetManifest.xml --target-org CI-Org --test-level RunLocalTests --ignore-warnings --ignore-conflicts --verbose
-    cat expressionSetManifest.xml
+    if [ -f "expressionSetManifest.xml" ]; then
+        cat expressionSetManifest.xml
+#        sf project deploy start --manifest expressionSetManifest.xml --target-org CI-Org --test-level RunLocalTests --ignore-warnings --ignore-conflicts --verbose
+    fi
 else
     echo "No differences, nothing to deploy."
 fi

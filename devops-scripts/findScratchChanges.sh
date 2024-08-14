ba973a609e0d33b0676f7fdca75565c76ee406b2
Kristina Klepik <kristygomel@gmail.com>
2024-08-14 14:24:40 +0200

    Edited

--- devops-scripts/findScratchChanges.sh
+++ devops-scripts/findScratchChanges.sh
@@ -4,62 +4,45 @@ current_branch=$1
 git fetch origin
 git stash push -m "temporary stash" && git checkout origin/develop
 
-#sf project retrieve start --metadata ExpressionSetDefinition --output-dir scratch_es --ignore-conflicts
+sf project retrieve start --metadata ExpressionSetDefinition --output-dir scratch_es --ignore-conflicts
 
-DIR1="scratch_es/main/default/expressionSetDefinition"
-all_expression_sets_dir="all_expression_sets"
+scratch_expression_sets_dir="scratch_es/main/default/expressionSetDefinition"
+branch_expression_sets_dir="all_expression_sets"
 
-# Create the directory to store combined files
-mkdir -p "$all_expression_sets_dir"
+# Create the directory to store expression_sets from all modules
+mkdir -p "$branch_expression_sets_dir"
 
 # Find all expressionSetDefinition directories and copy files to the combined directory
 src_dirs=$(find src -type d -name 'expressionSetDefinition')
-
 for dir in $src_dirs; do
-    echo "Processing directory: $dir"
-    # Copy files to the combined directory without preserving the parent directory structure
-    find "$dir" -type f -exec cp {} "$all_expression_sets_dir/" \;
+    find "$dir" -type f -exec cp {} "$branch_expression_sets_dir/" \;
 done
 
-# Compare DIR1 with the combined directory
-changed_files=$(diff -qr "$DIR1" "$all_expression_sets_dir" | grep -E '^Files ' | awk '{print $2, $4}' | sed "s|^$DIR1/||")
-
+changed_files=$(diff -qr "$scratch_expression_sets_dir" "$branch_expression_sets_dir" | grep -E '^Files ' | awk '{print $2, $4}' | sed "s|^$scratch_expression_sets_dir/||")
 # Extract base names without paths or file extensions and remove duplicates
-unique_names=$(echo "$changed_files" | sed -e 's|.*/||' -e 's/\.expressionSetDefinition-meta\.xml$//' | sort -u)
+expression_set_names=$(echo "$changed_files" | sed -e 's|.*/||' -e 's/\.expressionSetDefinition-meta\.xml$//' | sort -u)
 
 # Check if there are any changed files
-if [ -n "$unique_names" ]; then
-    echo "Differences found in:"
-    echo "$unique_names"
+if [ -n "$expression_set_names" ]; then
 
-    # Generate the manifest
     sf project generate manifest --metadata ExpressionSetDefinition --name expressionSetManifest
-
-    # Clean the manifest to include only changed files
     manifest_file="expressionSetManifest.xml"
-    temp_manifest="temp_manifest.xml"
 
-    # Copy the original manifest to a temporary file for editing
+    # Create copy of the original manifest to a temporary file for editing
+    temp_manifest="temp_manifest.xml"
     cp "$manifest_file" "$temp_manifest"
 
-    # Remove the wildcard <members>*</members> from the manifest
+    # Remove the <members> and add only changed
     sed -i '/<members>\*<\/members>/d' "$temp_manifest"
-    # Remove the <name>ExpressionSetDefinition</name> row and move it to the end of the types section
-    sed -i '/<name>ExpressionSetDefinition<\/name>/d' "$temp_manifest"
-
-    # Add changed files to the manifest
     for name in $unique_names; do
-        # Add the new member entry before the closing </types> tag
-        sed -i "/<\/types>/i <members>$name</members>" "$temp_manifest"
+      # Add file before <name> tag
+      sed -i "/<name>/i <members>$name</members>" "$temp_manifest"
     done
-    # Add the <name>ExpressionSetDefinition</name> entry back after all <members> entries
-    sed -i '/<\/types>/i <name>ExpressionSetDefinition<\/name>' "$temp_manifest"
-
 
     # Move the updated manifest to the original file
     mv "$temp_manifest" "$manifest_file"
 
-    # Deploy the changed files using the cleaned manifest
+    # Deploy the changed files to CI-Org
     if [ -f "$manifest_file" ]; then
         cat "$manifest_file"
         # Uncomment the following line to perform deployment

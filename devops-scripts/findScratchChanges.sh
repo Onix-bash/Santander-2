95f46f4c6c85b09249afac5a627b789e230d1b7f
Kristina Klepik <kristygomel@gmail.com>
2024-08-14 08:39:20 +0200

    Added

--- devops-scripts/findScratchChanges.sh
+++ devops-scripts/findScratchChanges.sh
@@ -7,4 +7,5 @@ DIR2="src/decision-centre/main/default/expressionSetDefinition"
 
 # Run the diff command and get the list of changed files
 changed_files=$(diff -qr "$DIR1" "$DIR2" | grep -E '^Files ' | awk '{print $2}' | sed "s|^$DIR1/||")
+echo "$changed_files"
 echo "changed_files=$changed_files" >> $GITHUB_ENV

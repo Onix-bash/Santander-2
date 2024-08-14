bee8563b171d1872753e65b803f09edaa0bd11e5
Kristina Klepik <kristygomel@gmail.com>
2024-08-14 09:59:12 +0200

    Edited

--- devops-scripts/findScratchChanges.sh
+++ devops-scripts/findScratchChanges.sh
@@ -5,6 +5,13 @@
 DIR1="scratch_es/main/default/expressionSetDefinition"
 DIR2="src/decision-centre/main/default/expressionSetDefinition"
 
+# Check if the retrieval command was successful
+if [ ! -d "$DIR1" ] || [ -z "$(ls -A "$DIR1")" ]; then
+    echo "No data retrieved from Salesforce or directory is empty."
+    echo "changed_files=" >> "$GITHUB_OUTPUT"
+    exit 0
+fi
+
 # Run the diff command and get the list of changed files
 changed_files=$(diff -qr "$DIR1" "$DIR2" | grep -E '^Files ' | awk '{print $2}' | sed "s|^$DIR1/||")
 

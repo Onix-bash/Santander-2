db6d3c445b7f4a5ebc64a0eab76c8f60ce7b426a
Kristina Klepik <kristygomel@gmail.com>
2024-08-20 14:08:33 +0200

    Update includeChangesESIntoPackage.sh

--- devops-scripts/includeChangesESIntoPackage.sh
+++ devops-scripts/includeChangesESIntoPackage.sh
@@ -1,45 +1,29 @@
 #!/bin/bash
 # $1 - PR was merged into <base> branch OR <release> branch was executed to be validated/deployed manually
-echo "This script was triggered by the $GITHUB_WORKFLOW workflow."
-echo "MANUAL_WORKFLOWS $MANUAL_WORKFLOWS"
-event_name=$GITHUB_EVENT_NAME
 
-# Output the event name
-echo "This workflow was triggered by the $event_name event."
-
-if [ -n "$MANUAL_WORKFLOWS" ]; then
-  echo "start"
-  IFS=$'\n' read -r -d '' -a MANUAL_WORKFLOW_ARRAY <<< "$(echo "$MANUAL_WORKFLOWS" | sed '/^\s*$/d' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')"
-  is_manual=false
-
-  for workflow in "${MANUAL_WORKFLOW_ARRAY[@]}"; do
-    if [[ "$workflow" == "$GITHUB_WORKFLOW" ]]; then
-       echo "$workflow == $GITHUB_WORKFLOW"
-      is_manual=true
-      break
-    fi
-  done
-echo "is_manual $is_manual"
- if $is_manual; then
-    # PR was merged into <base> branch
+GITHUB_EVENT_NAME=$GITHUB_EVENT_NAME
+echo "$GITHUB_EVENT_NAME"
+# Conditional logic based on the event
+if [ "$GITHUB_EVENT_NAME" = "push" ]; then
+    current_branch=""
+    source_to_check_changes="HEAD^"
+elif [ "$GITHUB_EVENT_NAME" = "workflow_dispatch" ]; then
     current_branch="origin/$(git branch --show-current)"
     source_to_check_changes="origin/$(git remote show origin | grep 'HEAD branch' | sed 's/.*: //')"
- else
+else
     current_branch="origin/$GITHUB_HEAD_REF"
     source_to_check_changes="origin/$GITHUB_BASE_REF"
- fi
-
-
-  echo "current_branch= $current_branch"
-  echo "source_to_check_changes= $source_to_check_changes"
-  ES_PATH='^src/.*/expressionSetDefinition/'
-
-  git fetch origin
-  changed_es_files=$(git diff --name-only $source_to_check_changes...$current_branch | grep -E $ES_PATH)
-  echo "changed_es_files $changed_es_files"
-  for file_path in $changed_es_files; do
-    echo "!$file_path" >> .forceignore
-  done
-
-
 fi
+
+
+echo "current_branch= $current_branch"
+echo "source_to_check_changes= $source_to_check_changes"
+ES_PATH='^src/.*/expressionSetDefinition/'
+
+git fetch origin
+changed_es_files=$(git diff --name-only $source_to_check_changes...$current_branch | grep -E $ES_PATH)
+echo "changed_es_files $changed_es_files"
+for file_path in $changed_es_files; do
+  echo "!$file_path" >> .forceignore
+done
+

22cf7e77103c922960067359d851f4c4daa1d80c
Kristina Klepik <kristygomel@gmail.com>
2024-08-20 13:38:17 +0200

    Update includeChangesESIntoPackage.sh

--- devops-scripts/includeChangesESIntoPackage.sh
+++ devops-scripts/includeChangesESIntoPackage.sh
@@ -5,9 +5,10 @@ echo "MANUAL_WORKFLOWS $MANUAL_WORKFLOWS"
 if [ -n "$MANUAL_WORKFLOWS" ]; then
   IFS=$'\n' read -r -d '' -a MANUAL_WORKFLOW_ARRAY <<< "$(echo "$MANUAL_WORKFLOWS" | sed '/^\s*$/d' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')"
   is_manual=false
-  echo "${MANUAL_WORKFLOW_ARRAY[@]}"
+
   for workflow in "${MANUAL_WORKFLOW_ARRAY[@]}"; do
     if [[ "$workflow" == "$GITHUB_WORKFLOW" ]]; then
+       echo "$workflow == $GITHUB_WORKFLOW"
       is_manual=true
       break
     fi
@@ -26,11 +27,14 @@ if [ -n "$MANUAL_WORKFLOWS" ]; then
    source_to_check_changes="origin/$(git remote show origin | grep 'HEAD branch' | sed 's/.*: //')"
  fi
 
+
+  echo "current_branch= $current_branch"
+  echo "source_to_check_changes= $source_to_check_changes"
   ES_PATH='^src/.*/expressionSetDefinition/'
 
   git fetch origin
   changed_es_files=$(git diff --name-only $source_to_check_changes...$current_branch | grep -E $ES_PATH)
-
+  echo "changed_es_files $changed_es_files"
   for file_path in $changed_es_files; do
     echo "!$file_path" >> .forceignore
   done
